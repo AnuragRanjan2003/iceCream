77a9e5949e017a1aa28f1e13a776c1c6d44f3ba0
AnuragRanjan2003 <anuragranjan1261@gmail.com>
2023-03-19 14:27:00 +0530

    commit

--- index.js
+++ index.js
@@ -1,43 +1,14 @@
 import express from 'express';
 import bodeParser from 'body-parser';
 import routes from './routes/request.js';
-import cj from 'circular-json';
+
 
 const app = express();
 const port  = 5000;
-const context = "You are an AI assistant that is expert in ice creams.\nYou know about all kinds of ice cream flavours ans how to make them.\nYou can provide advice on ice creams flavours ,the ingredients needed to make them ans any thing else related to ice creams.\nIf you are unable to provide an answer to a question, please respond with \"I don't know about that.\".\nDo not use external URLs in your answers.Do not refer to any blogs in your answers.\nFormat any lists on individual lines with a bullet in front of each of  them. \n"
+
 
 app.use(bodeParser.json());
 app.use('/request',routes);
-import { Configuration, OpenAIApi } from 'openai';
-
-const configuration = new Configuration({
-  apiKey: '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',
-});
-const openai = new OpenAIApi(configuration);
-
-
-async function getResponse(prompt){
-const response = await openai.createCompletion({
-  model: "text-davinci-003",
-  prompt: prompt,
-  temperature: 0.7,
-  max_tokens: 256,
-  top_p: 1,
-  frequency_penalty: 0,
-  presence_penalty: 0,
-});
-
-return response;
-}
-
-app.get("/",(req,res)=>{
-    getResponse(context+"Human:tell me about ice cream flavours.").then((it)=>{
-        const str=  cj.stringify(it);
-        res.send(JSON.parse(str));
-    });
-    //res.send('hi');
-});
 
 app.listen(port,()=>{
     console.log('I am live at http://localhost:5000');
--- routes/request.js
+++ routes/request.js
@@ -1,8 +1,45 @@
 import express from 'express'
+import { Configuration, OpenAIApi } from 'openai';
+import cj from 'circular-json';
 const router = express.Router();
+const context = "You are an AI assistant that is expert in ice creams.\nYou know about all kinds of ice cream flavours ans how to make them.\nYou can provide advice on ice creams flavours ,the ingredients needed to make them ans any thing else related to ice creams.\nIf you are unable to provide an answer to a question, please respond with \"I don't know about that.\".\nDo not use external URLs in your answers.Do not refer to any blogs in your answers.\nFormat any lists on individual lines with a bullet in front of each of  them. \n"
 
 router.get("/",(req ,res)=>{
     res.send('Hello');
 });
+
+
+const configuration = new Configuration({
+  apiKey: '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',
+});
+const openai = new OpenAIApi(configuration);
+
+
+async function getResponse(prompt){
+const response = await openai.createCompletion({
+  model: "text-davinci-003",
+  prompt: prompt,
+  temperature: 0.7,
+  max_tokens: 256,
+  top_p: 1,
+  frequency_penalty: 0,
+  presence_penalty: 0,
+});
+
+return response;
+}
+
+router.get("/icecream",(req,res)=>{
+    console.log(req.query.text);
+    getResponse(context+"Human:"+req.query.text+"\n").then((it)=>{
+        const str = cj.stringify(it);
+        const result = JSON.parse(str);
+
+        res.send(result.data);
+    });
+    //res.send(req.query.q);
+});
+
+
 
 export default router;
