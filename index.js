f56d3285dc9bee92dd6d6bade87a7525c0782d7a
AnuragRanjan2003 <anuragranjan1261@gmail.com>
2023-03-25 13:31:46 +0530

    nodemon

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 import express from 'express';
 import bodeParser from 'body-parser';
 import routes from './routes/request.js';
+import ngrok from 'ngrok';
 
 
 const app = express();
 const port  = 5000;
-
+const url = await ngrok.connect()
+    .then(url=>{
+        console.log(url);
+    })
+    .catch(err=> console.error(err));  //http://localhost:80
 
 app.use(bodeParser.json());
 app.use('/request',routes);
--- routes/request.js
+++ routes/request.js
@@ -2,8 +2,8 @@ import express from 'express'
 import { Configuration, OpenAIApi } from 'openai';
 import cj from 'circular-json';
 const router = express.Router();
-const context = "You are an AI assistant that is expert in ice creams.\nYou know about all kinds of ice cream flavours ans how to make them.\nYou can provide advice on ice creams flavours ,the ingredients needed to make them ans any thing else related to ice creams.\nIf you are unable to provide an answer to a question, please respond with \"I don't know about that.\".\nDo not use external URLs in your answers.Do not refer to any blogs in your answers.\nFormat any lists on individual lines with a bullet in front of each of  them. \n"
-
+const base_context = "You are an AI assistant that is expert in ice creams.\nYou know about all kinds of ice cream flavours ans how to make them.\nYou can provide advice on ice creams flavours ,the ingredients needed to make them ans any thing else related to ice creams.\nIf you are unable to provide an answer to a question, please respond with \"I don't know about that.\".\nDo not use external URLs in your answers.Do not refer to any blogs in your answers.\nFormat any lists on individual lines with a bullet in front of each of  them. \n";
+var context = base_context;
 router.get("/",(req ,res)=>{
     res.send('Hello');
 });
@@ -31,13 +31,21 @@ return response;
 
 router.get("/icecream",(req,res)=>{
     console.log(req.query.text);
-    getResponse(context+"Human:"+req.query.text+"\n").then((it)=>{
+    context = context + "Human:"+req.query.text+"\n";
+    getResponse(context).then((it)=>{
         const str = cj.stringify(it);
         const result = JSON.parse(str);
 
         res.send(result.data);
+        console.log(result.data.choices[0].text);
+        context = context+result.data.choices[0].text;
     });
     //res.send(req.query.q);
+});
+
+router.get("/clear",(req,res)=>{ 
+    context = base_context;
+    res.send("context cleared");
 });
 
 
